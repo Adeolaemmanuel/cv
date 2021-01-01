4cce85df0f43cef1fe42948440bec739d083f911
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-01 13:40:49 +0100

    Modified files

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -64,8 +64,8 @@ class Sidebar extends Component{
     render() {
         if(window.matchMedia("(max-width: 767px)").matches){
             return (
-                <div style={{position:'fixed', bottom: '0'}}>
-                    <div className='w3-bar w3-card w3-container w3-white w3-margin-right w3-padding'>
+                <div>
+                    <div className='w3-bar w3-card w3-bottom w3-container w3-white w3-margin-right w3-padding'>
                         <Link to='Admin'><img src={dash} alt={dash} className='svg w3-padding w3-bar-item' /></Link>
                         <Link to='Add'><img src={add} alt={add} className='svg w3-padding w3-bar-item' /></Link>
                         <Link to='Mail'><img src={mail} alt={mail} className='svg w3-padding w3-bar-item ' /></Link>
